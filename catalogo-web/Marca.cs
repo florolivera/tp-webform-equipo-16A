e082d9846035b734b5dcfb10a2503c37b450dd86
Florencia <117230347+florolivera@users.noreply.github.com>
2025-10-05 18:08:13 -0300

    add objetos

---
+++ catalogo-web/Marca.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace catalogo_web
+{
+    public class Marca
+    {
+    }
+}
---
+++ dominio/Voucher.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Voucher
+    {
+        public string CodigoVoucher { get; set; }
+        public int? IdCliente { get; set; }
+        public int? IdArticulo { get; set; }
+        public DateTime? FechaCanje { get; set; }
+
+        public bool Usado
+        {
+            get
+            {
+                return IdCliente.HasValue || IdArticulo.HasValue || FechaCanje.HasValue;
+            }
+        }
+
+        public override string ToString()
+        {
+            return CodigoVoucher;
+        }
+    }
+}
